d1cb1c15bfa5734886289692c8977982e5ea04d7
huynguyen294 <anhhuy2099@gmail.com>
2022-03-07 13:43:11 +0700

    add project 20

---
+++ 20_button_effect/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', (ev) => {
+        const x = ev.clientX
+        const y = ev.clientY
+    
+        const buttonTop = ev.target.offsetTop
+        const buttonLeft = ev.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        ev.target.appendChild(circle)
+
+        setTimeout(()=>circle.remove(),500)
+        
+    })  
+})
